c9b4605824e6dffae510a9700262278460c389d1
Anastasiya Niadbailik <anastasiya.niadbailik@gmail.com>
2024-08-14 17:17:27 +0200

    feat: lesson (pet api)

--- abra_api/cypress/e2e/register_pet_api.cy.js
+++ abra_api/cypress/e2e/register_pet_api.cy.js
@@ -8,6 +8,8 @@ describe('register', () => {
             'email': randomEmail,
             'password': '1234456',
             'confirm_password': '1234456'
+        }).then(({body}) => {
+            for (let key of Object.keys(body)) cy.log(key, body[key])
         })
         cy.log('Generated email address:', randomEmail);
     })
