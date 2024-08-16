f89f690ea367985011e62618e39c131d0fbdf15d
Anastasiya Niadbailik <anastasiya.niadbailik@gmail.com>
2024-08-16 17:17:13 +0200

    feat: lesson (pet api)

--- abra_api/cypress/e2e/register_pet_api.cy.js
+++ abra_api/cypress/e2e/register_pet_api.cy.js
@@ -1,17 +1,20 @@
 import { generateRandomEmail, generateRandomPassword } from '../utils/data';
-import RegisterResponse from '../utils/api_models';
+import RegisterResponse from '../models/api_models';
 
 describe('register', () => {
     it('positive', () => {
         const randomEmail = generateRandomEmail();
+        cy.log('Generated email address:', randomEmail);
         cy.request('POST', '/register' , {
             'email': randomEmail,
             'password': '1234456',
             'confirm_password': '1234456'
-        }).then(({body}) => {
+        }).then(({ body }) => {
+            cy.log(JSON.stringify(body)) 
             expect(RegisterResponse.compare_models(body)).to.equal(true);
+            // Проверка наличия email в теле ответа
+            expect(body).to.have.property('email', randomEmail);
         })
-        cy.log('Generated email address:', randomEmail);
     })
 
     it('wrong password', () => {
@@ -26,6 +29,7 @@ describe('register', () => {
                 'confirm_password': '4321'
         }
     }).then(response => {
+        cy.log(JSON.stringify(response)) // Вывод полного объекта response в логи
         expect(response['status']).to.equal(400);
         // for (let key of Object.keys(body)) cy.log(key, body[key]);
         expect(RegisterResponse.compare_models(response['body'], false)).to.equal(true);
