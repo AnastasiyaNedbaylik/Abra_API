c3add9850c6947294dcb2cff06f35c87f8b754fe
Anastasiya Niadbailik <anastasiya.niadbailik@gmail.com>
2024-08-17 13:25:17 +0200

    fix: edit tests

--- abra_api/cypress/models/api_models_abra.js
+++ abra_api/cypress/models/api_models_abra.js
@@ -29,32 +29,3 @@ class RegisterResponse{
 }
 
 module.exports = new RegisterResponse();
-
-module.exports = new RegisterResponse();
-
-    // compare_models(model, positive = true) {
-    //     let expectedModel = (positive) ? this.response : this.negative_response;
-    
-    //     function deepEqual(obj1, obj2) {
-    //         if (typeof obj1 !== 'object' || obj1 === null || typeof obj2 !== 'object' || obj2 === null) {
-    //             return obj1 === obj2;
-    //         }
-    
-    //         let keys1 = Object.keys(obj1);
-    //         let keys2 = Object.keys(obj2);
-    
-    //         if (keys1.length !== keys2.length) {
-    //             return false;
-    //         }
-    
-    //         for (let key of keys1) {
-    //             if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
-    //                 return false;
-    //             }
-    //         }
-    
-    //         return true;
-    //     }
-    
-    //     return deepEqual(model, expectedModel);
-    // }
