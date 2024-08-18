961777fbea6eaff628d1c034fb3b11cc2a215665
Anastasiya Niadbailik <anastasiya.niadbailik@gmail.com>
2024-08-18 20:35:27 +0200

    feat: add negative tests

--- abra_api/cypress/e2e/login_api.cy.js
+++ abra_api/cypress/e2e/login_api.cy.js
@@ -1,6 +1,6 @@
 import { generateRandomEmail, generateRandomPassword } from '../utils/data';
 import LoginResponse from '../models/api_models_abra';
-import СonfirmationResponse from '../models/api_models_abra';
+import RegisterResponse from '../models/api_models_abra';
 import { invalidEmails, invalidPasswords } from '../utils/data';
 import { confirmEmailRegistration } from '../utils/registration';
 
@@ -46,6 +46,7 @@ describe('Login', () => {
             }).then(loginResponse => {
                 cy.log(JSON.stringify(loginResponse));
                 expect(loginResponse.status).to.equal(422);
+                expect(loginResponse.headers).to.have.property('content-type', 'application/json');
                 expect(LoginResponse.compare_models(loginResponse.body, false)).to.equal(true);
             });
         });
@@ -67,12 +68,13 @@ describe('Login', () => {
             }).then(loginResponse => {
                 cy.log(JSON.stringify(loginResponse));
                 expect(loginResponse.status).to.equal(422);
+                expect(loginResponse.headers).to.have.property('content-type', 'application/json');
                 expect(LoginResponse.compare_models(loginResponse.body, false)).to.equal(true);
             });
         });
     });
 
-    it('login registred email and incorrect password)', () => {
+    it('login with registred email and incorrect password', () => {
         // Создаем временный email
         cy.task('createTemporaryEmail').then((emailAddress) => {
             // Подтверждаем регистрацию
@@ -88,8 +90,50 @@ describe('Login', () => {
                 }).then((loginResponse) => {
                     cy.log(JSON.stringify(loginResponse));
                     expect(loginResponse.status).to.equal(403);
+                    expect(loginResponse.headers).to.have.property('content-type', 'application/json');
                     expect(loginResponse.body).to.have.property('detail', 'Wrong email or password, maybe email was not confirmed or account was deleted?');
                 });
+            });
+        });
+    });
+
+    it('login with registred but not confirmed email', () => {
+        const randomEmail = generateRandomEmail();
+        cy.log('Generated email address:', randomEmail);
+        const randomPassword = generateRandomPassword();
+        cy.log('Generated password:', randomPassword);
+
+        // Первый запрос для регистрации
+        cy.request('POST', '/auth/sign-up/supplier', {
+            'email': randomEmail,
+            'password': randomPassword
+        }).then(response => {
+            cy.log('First registration response:', JSON.stringify(response));
+            expect(response.status).to.equal(200);
+            expect(RegisterResponse.compare_models(response['body'], true)).to.equal(true);
+    
+            // Добавление задержки перед повторным запросом, чтобы избежать ошибки
+            // 'Database error: could not serialize access due to read/write dependencies among transactions'
+            cy.wait(2000);
+
+            //Попытка логина с тем же email и password
+            const existingEmail = randomEmail;
+            cy.log('Attempting to login with the same email:', existingEmail);
+            const existingPassword = randomPassword;
+            cy.log('Attempting to login with the same password:', existingPassword)
+            cy.request({
+                method: 'POST',
+                url: '/auth/sign-in',
+                failOnStatusCode: false,
+                body: {
+                    'email': existingEmail,
+                    'password': existingPassword
+                }
+            }).then(loginResponse => {
+                cy.log('Second registration response:', JSON.stringify(loginResponse));
+                expect(loginResponse.status).to.equal(403);
+                expect(loginResponse.headers).to.have.property('content-type', 'application/json');
+                expect(loginResponse.body).to.have.property('detail', 'Wrong email or password, maybe email was not confirmed or account was deleted?');
             });
         });
     });
