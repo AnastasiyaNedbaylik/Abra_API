b92ce56f5664840423a8f9fab7b5ca0c05e0b7fb
Anastasiya Niadbailik <anastasiya.niadbailik@gmail.com>
2024-08-18 20:23:17 +0200

    feat: add negative tests

--- abra_api/cypress/e2e/login_api.cy.js
+++ abra_api/cypress/e2e/login_api.cy.js
@@ -1,4 +1,4 @@
-import { generateRandomEmail } from '../utils/data';
+import { generateRandomEmail, generateRandomPassword } from '../utils/data';
 import LoginResponse from '../models/api_models_abra';
 import СonfirmationResponse from '../models/api_models_abra';
 import { invalidEmails, invalidPasswords } from '../utils/data';
@@ -11,16 +11,84 @@ describe('Login', () => {
         cy.task('createTemporaryEmail').then((emailAddress) => {
             // Подтверждаем регистрацию
             return confirmEmailRegistration(emailAddress).then((password) => {
-                cy.request('POST', '/auth/sign-in', {
-                    'email': emailAddress,
-                    'password': password
+                cy.request({
+                    method: 'POST', 
+                    url: '/auth/sign-in', 
+                    body: {
+                        'email': emailAddress,
+                        'password': password
+                    }
                 }).then((loginResponse) => {
                     cy.log(JSON.stringify(loginResponse));
                     expect(loginResponse.status).to.equal(200);
-                    expect(LoginResponse.compare_models(loginResponse['body'], true)).to.equal(true);
                     // Проверка заголовков ответа
                     expect(loginResponse.headers).to.have.property('content-type', 'application/json');
                     expect(loginResponse.headers['set-cookie']).to.be.an('array').and.have.lengthOf(2);
+                    expect(LoginResponse.compare_models(loginResponse['body'], true)).to.equal(true);
+                });
+            });
+        });
+    });
+
+    it('login with invalid email', () => {
+        const randomPassword = generateRandomPassword();
+        cy.log('Generated password:', randomPassword);
+        cy.wrap(invalidEmails).each((email) => {
+            cy.log(`Testing with email: ${email}`);
+            cy.request({
+                method: 'POST',
+                url: '/auth/sign-in',
+                failOnStatusCode: false,
+                body: {
+                    'email': email,
+                    'password': randomPassword
+                }
+            }).then(loginResponse => {
+                cy.log(JSON.stringify(loginResponse));
+                expect(loginResponse.status).to.equal(422);
+                expect(LoginResponse.compare_models(loginResponse.body, false)).to.equal(true);
+            });
+        });
+    });
+
+    it('login with invalid password', () => {
+        const randomEmail = generateRandomEmail();
+        cy.log('Generated email address:', randomEmail);
+        cy.wrap(invalidPasswords).each((password) => {
+            cy.log(`Testing with password: ${password}`);
+            cy.request({
+                method: 'POST',
+                url: '/auth/sign-in',
+                failOnStatusCode: false,
+                body: {
+                    'email': password,
+                    'password': randomEmail
+                }
+            }).then(loginResponse => {
+                cy.log(JSON.stringify(loginResponse));
+                expect(loginResponse.status).to.equal(422);
+                expect(LoginResponse.compare_models(loginResponse.body, false)).to.equal(true);
+            });
+        });
+    });
+
+    it('login registred email and incorrect password)', () => {
+        // Создаем временный email
+        cy.task('createTemporaryEmail').then((emailAddress) => {
+            // Подтверждаем регистрацию
+            return confirmEmailRegistration(emailAddress).then((password) => {
+                cy.request({
+                    method: 'POST', 
+                    url: '/auth/sign-in', 
+                    failOnStatusCode: false,
+                    body: {
+                        'email': emailAddress,
+                        'password': 'incorrect'
+                    }
+                }).then((loginResponse) => {
+                    cy.log(JSON.stringify(loginResponse));
+                    expect(loginResponse.status).to.equal(403);
+                    expect(loginResponse.body).to.have.property('detail', 'Wrong email or password, maybe email was not confirmed or account was deleted?');
                 });
             });
         });
